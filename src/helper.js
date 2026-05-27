77844d145caaacb50115b7c34f9364ee36ea2d07
amandaaaaaaew23 <amandargbys@gmail.com>
2026-05-27 21:56:21 +0700

    refactor: streamline utility functions

--- src/helper.js
+++ src/helper.js
@@ -10,3 +10,4 @@
 // update 35 at Rab 27 Mei 2026 20:21:01 WIB
 // update 36 at Rab 27 Mei 2026 20:25:35 WIB
 // update 55 at Rab 27 Mei 2026 21:52:20 WIB
+// update 56 at Rab 27 Mei 2026 21:56:21 WIB
