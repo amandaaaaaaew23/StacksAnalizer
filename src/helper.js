2981621a407065e94cc0c75c241d1b06b6a557e9
amandaaaaaaew23 <amandargbys@gmail.com>
2026-05-28 08:20:19 +0700

    fix: handle edge case scenario

--- src/helper.js
+++ src/helper.js
@@ -35,3 +35,4 @@
 // update 183 at Kam 28 Mei 2026 07:36:31 WIB
 // update 184 at Kam 28 Mei 2026 07:41:18 WIB
 // update 190 at Kam 28 Mei 2026 08:10:47 WIB
+// update 192 at Kam 28 Mei 2026 08:20:19 WIB
