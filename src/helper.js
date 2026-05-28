92b183c83b0a8a9339639b1a0b299d1c4e835df6
amandaaaaaaew23 <amandargbys@gmail.com>
2026-05-29 00:10:02 +0700

    feat: enhance contract interaction layer

--- src/helper.js
+++ src/helper.js
@@ -64,3 +64,4 @@
 // update 134 at Kam 28 Mei 2026 21:44:38 WIB
 // update 141 at Kam 28 Mei 2026 22:15:39 WIB
 // update 164 at Jum 29 Mei 2026 00:01:05 WIB
+// update 166 at Jum 29 Mei 2026 00:10:02 WIB
