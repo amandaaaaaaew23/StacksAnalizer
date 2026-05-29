842a3c80d7cbd10f4123abd511ea6dd02d74888b
amandaaaaaaew23 <amandargbys@gmail.com>
2026-05-29 09:52:25 +0700

    feat: introduce helper abstraction layer

--- src/helper.js
+++ src/helper.js
@@ -79,3 +79,4 @@
 // update 274 at Jum 29 Mei 2026 08:23:35 WIB
 // update 278 at Jum 29 Mei 2026 08:45:33 WIB
 // update 290 at Jum 29 Mei 2026 09:38:52 WIB
+// update 293 at Jum 29 Mei 2026 09:52:25 WIB
