d7223dc2ca5a9c7e6bdf8e564c33f2299b53a0e1
amandaaaaaaew23 <amandargbys@gmail.com>
2026-05-29 22:38:51 +0700

    docs: improve architecture documentation clarity

--- src/helper.js
+++ src/helper.js
@@ -105,3 +105,4 @@
 // update 124 at Jum 29 Mei 2026 21:32:43 WIB
 // update 131 at Jum 29 Mei 2026 22:04:22 WIB
 // update 132 at Jum 29 Mei 2026 22:07:47 WIB
+// update 139 at Jum 29 Mei 2026 22:38:51 WIB
