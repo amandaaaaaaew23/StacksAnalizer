7069f841cc466c779bf53e34342a87a05e304aa3
amandaaaaaaew23 <amandargbys@gmail.com>
2026-05-30 13:45:50 +0700

    refactor: improve API integration flow

--- src/helper.js
+++ src/helper.js
@@ -139,3 +139,4 @@
 // update 3 at Sab 30 Mei 2026 12:12:11 WIB
 // update 4 at Sab 30 Mei 2026 12:17:20 WIB
 // update 14 at Sab 30 Mei 2026 13:05:55 WIB
+// update 22 at Sab 30 Mei 2026 13:45:50 WIB
