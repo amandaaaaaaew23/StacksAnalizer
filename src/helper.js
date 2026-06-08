306a43d4379ce2a57a2dd00f1b96a115cfd27fdd
amandaaaaaaew23 <amandargbys@gmail.com>
2026-06-09 04:07:09 +0700

    fix: adjust UI rendering issue

--- src/helper.js
+++ src/helper.js
@@ -204,3 +204,4 @@
 // update 18 at Sel 09 Jun 2026 01:01:51 WIB
 // update 19 at Sel 09 Jun 2026 01:17:08 WIB
 // update 28 at Sel 09 Jun 2026 03:34:08 WIB
+// update 30 at Sel 09 Jun 2026 04:07:09 WIB
