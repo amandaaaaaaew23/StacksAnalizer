b2627a291855a4ce16aa90b3c321fdabc39765da
amandaaaaaaew23 <amandargbys@gmail.com>
2026-06-10 17:45:43 +0700

    docs: expand security documentation

--- src/helper.js
+++ src/helper.js
@@ -223,3 +223,4 @@
 // update 1 at Rab 10 Jun 2026 12:19:31 WIB
 // update 5 at Rab 10 Jun 2026 13:24:27 WIB
 // update 8 at Rab 10 Jun 2026 14:07:49 WIB
+// update 18 at Rab 10 Jun 2026 17:45:43 WIB
