171ff5927177eb150c556701b16f9db74470f38e
amandaaaaaaew23 <amandargbys@gmail.com>
2026-06-16 03:48:20 +0700

    feat: implement contract utility abstraction

--- src/helper.js
+++ src/helper.js
@@ -263,3 +263,4 @@
 // update 20 at Sen 15 Jun 2026 22:51:54 WIB
 // update 27 at Sel 16 Jun 2026 00:44:04 WIB
 // update 32 at Sel 16 Jun 2026 01:57:55 WIB
+// update 40 at Sel 16 Jun 2026 03:48:20 WIB
